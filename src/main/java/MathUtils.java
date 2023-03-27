13055adc2298bcc1af2ea210aa427fb57eac2ed6
Shunbao Chen <samballchen@hotmail.com>
2023-03-26 20:54:00 -0700

    New MathUtils.java file

---
+++ src/main/java/MathUtils.java
@@ -0,0 +1,21 @@
+
+import System;
+
+public class MathUtils {
+    public static int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static void Main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("GCD(10, 24)=", gcd(10, 24));
+        System.out.println("LCM(10, 24)=", lcm(10, 24));
+    }
+}
