639105e29359096815ad48cf93c629192291962c
Jinghang Chen <chenjinghang123434@gmail.com>
2023-07-07 11:14:00 -0700

    Revised Format and Overfill and Decimal Support

--- codelab/JinghangCodeLab/MagicNumberConverter.java
+++ codelab/JinghangCodeLab/MagicNumberConverter.java
@@ -2,40 +2,52 @@ package JinghangCodeLab;
 
 public class MagicNumberConverter {
 
-    public static int binaryToDecimal(String binary)
-    {  
-        //edge cases
-        if (binary == "0"){
+    public static int binaryToDecimal(String binary) {
+        if (Double.parseDouble(binary) > Integer.MAX_VALUE){
+            System.out.println("Out of Range not in Integer Range");
+            return 0;
+        }
+        // Edge cases
+        if (binary == "0") {
             return 0;
         }
 
-        //negative
-        int isNegative = -1;
-        if(Integer.parseInt(binary) < 0){
-            isNegative = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary.charAt(i) == '.') {
+                System.out.println("Cannot Input Decimals");
+                return 0;
+            }
         }
-        else{
-            isNegative = -1;
+
+        // Negative check
+        boolean isNegative;
+        if (binary.charAt(0) == '-') {
+            isNegative = true;
+        } else {
+            isNegative = false;
         }
-        //formula
-        int length =  binary.length() - 1;
+
+        // Formula
+        int length = binary.length() - 1;
         int multiple = 1;
         int sum = 0;
-        for (int i = length; i > isNegative; i--){
-            char place = binary.charAt(i);
-            int num = Character.getNumericValue(place);
+        int subtractIfNegative;
+        if(isNegative == true){
+            subtractIfNegative = 0;
+        } else{
+            subtractIfNegative = -1;
+        }
+        for (int i = length; i > subtractIfNegative; i--) {
+            int num = Character.getNumericValue(binary.charAt(i));
             int val = multiple * num;
             sum += val;
             multiple *= 2;
         }
-        if(isNegative == 0){
-            return sum*-1;
+
+        if (isNegative == true) {
+            return -sum;
         }
 
-
-        
-    
         return sum;
     }
-    
 }
--- codelab/JinghangCodeLab/MagicNumberConverterTest.java
+++ codelab/JinghangCodeLab/MagicNumberConverterTest.java
@@ -8,9 +8,13 @@ public class MagicNumberConverterTest {
     @Test
     public void testConvertBinaryNumber() {
         MagicNumberConverter convert = new MagicNumberConverter();
-                assertEquals(-10,convert.binaryToDecimal("-1010"));
+        assertEquals(0,convert.binaryToDecimal("11111111111111111111111111111111111111111111111111"));
+        assertEquals(10,convert.binaryToDecimal("1010"));
+        assertEquals(-10,convert.binaryToDecimal("-1010"));
+        assertEquals(0,convert.binaryToDecimal("0"));
+        assertEquals(0,convert.binaryToDecimal("0.1010001"));
 
-        
+
     }
     
 }
